c0cb999bf3c0cfd1fd90b54a312aca82b9d1ba63
xu.nan <634295462@qq.com>
2017-03-30 17:31:10 +0800

    uuiuuui

--- app/src/main/java/org/ia/transporter/fragment/HomeFragment.java
+++ app/src/main/java/org/ia/transporter/fragment/HomeFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import org.ia.transporter.R;
+import org.xutils.x;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +25,9 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        x.view().inject(this, v);
+        return v;
     }
 
 }
